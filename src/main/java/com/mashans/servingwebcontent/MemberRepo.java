503ccb3c45a92c7f9d9c41c6a4353e2f47c19ab5
fadi.mashan <fadi.mashan@gmail.com>
2021-06-09 14:42:47 +0200

    Add member details page

--- src/main/java/com/mashans/servingwebcontent/MemberRepo.java
+++ src/main/java/com/mashans/servingwebcontent/MemberRepo.java
@@ -3,5 +3,5 @@ package com.mashans.servingwebcontent;
 import com.mashans.servingwebcontent.Member;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepo extends CrudRepository<Member, String> {
+public interface MemberRepo extends CrudRepository<Member, Long> {
 }
--- src/main/java/com/mashans/servingwebcontent/VehiclesController.java
+++ src/main/java/com/mashans/servingwebcontent/VehiclesController.java
@@ -3,10 +3,13 @@ package com.mashans.servingwebcontent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 public class VehiclesController {
@@ -24,8 +27,17 @@ public class VehiclesController {
 //        return "vehicles";
 //    }
 
+    @RequestMapping("/vehicle/{id}")
+    public String memberDetails(@PathVariable Long id, Model model) {
+        model.addAttribute("vehicle", vRepo.findById(id));
+        //model.addAttribute("member", mRepo.findAll());
+        Member m1 = vRepo.findById(id).get().getMembers().get(0);
+        model.addAttribute("vehicle", m1);
+        return "vehicle";
+    }
 
-    @RequestMapping(value = "/vehicles", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String vehiclesList(Model model){
         model.addAttribute("vehicles", vRepo.findAll());
         Iterable<Vehicle> lis = vRepo.findAll();
